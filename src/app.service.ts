8340baf4ce0f963e2c8207726690d1c7a8a85777
dejongyeong <dejong.re2@gmail.com>
2025-03-18 18:19:54 +0000

    feat: create configuration to read from .env

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,11 @@ import { Injectable } from "@nestjs/common";
 
 @Injectable()
 export class AppService {
+  // constructor(private readonly config: ConfigService) {}
+
   getHello(): string {
+    // const port = this.config.get<number>("PORT");
+
     return "Hello World!";
   }
 }
