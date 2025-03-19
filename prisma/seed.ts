fca6d44f33ca5e922fc1784a7ba8286af4126c20
dejongyeong <dejong.re2@gmail.com>
2025-03-19 19:41:32 +0000

    refactor: add keywords field to publication in db

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -17,7 +17,7 @@ async function main() {
       abstract:
         "Industry 4.0 or fourth industrial revolution elevates the computerization of Industry 3.0 and enhances it with smart and autonomous systems driven by data and Machine Learning. This paper reviews the advantages and disadvantages of sensors and the architecture of multi-sensor setup for object detection. Here we consider the case of autonomous systems in for large heavy vehicles off-road in industrial environments with the use of camera sensor, LiDAR sensor, and radar sensor. Understanding the vehicles surroundings is a vital task in autonomous operation where personnel and other obstacles present significant hazard of collision. This paper review further discusses the challenges of time synchronisation on sensor data acquisition in multi-modal sensor fusion for personnel and object detection, and details a solution implemented in a Python environment.",
       mainAuthor: "De Jong Yeong",
-      CoAuthors: ["John Barry", "Joseph Walsh"],
+      coAuthors: ["John Barry", "Joseph Walsh"],
       link: "https://ieeexplore.ieee.org/document/9180186",
       publisher: "IEEE",
       published: true,
